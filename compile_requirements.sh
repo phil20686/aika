349e7944f98fa44fb8dec13d490d5f88c6564ac7
Dominic Kennedy <dominic.cm.kennedy@gmail.com>
2022-04-20 13:22:23 +0100

    add requirements file + compilation script

---
+++ compile_requirements.sh
@@ -0,0 +1,29 @@
+set -e
+
+# first, build wheels for all the packages, but exclude dependencies so that the
+# packages don't need to know where to find each other.
+for package_dir in libs/*
+do
+  cd $package_dir
+  pip wheel --no-deps . -w ../../wheels
+  cd -
+done
+
+# now that we have populated `wheels` with wheels for each package, we can point to it
+# with --find-links to ensure that each package can resolve all of its local
+# dependencies.
+
+# I actually think that this step is unnecessary for our needs? But I think it might be
+# useful for deployments, so I'll keep it commented out for now.
+
+#for package_dir in libs/*
+#do
+#  cd $package_dir
+#  pip wheel . -w ../../wheels --find-links ../../wheels
+#  cd -
+#done
+
+pip-compile requirements.in --find-links ./wheels -q
+
+# replace all references to the local packages with editable path versions
+sed -i "" -E "s|^ebony-([^=]+)==.*$|-e libs/\1|g" requirements.txt
