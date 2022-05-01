64d5ae9f75fc1c6998490902f9b42ef8f32e6d38
Dominic Kennedy <dominic.cm.kennedy@gmail.com>
2022-05-01 14:30:12 +0100

    Add equals method to handle pandas/numpy equality

--- libs/utilities/src/ebony/utilities/pandas_utils.py
+++ libs/utilities/src/ebony/utilities/pandas_utils.py
@@ -1,6 +1,23 @@
+import numpy as np
+import pandas as pd
 from typing import TypeVar
 from pandas import Index, DataFrame, Series
 
 # these types can be used to make functions that return the same pandas type as they are given.
 Tensor = TypeVar("Tensor", DataFrame, Series)
 IndexTensor = TypeVar("IndexTensor", DataFrame, Series, Index)
+
+
+def equals(left, right) -> bool:
+    """
+    General-purpose equality method which works properly with pandas tensors and numpy
+    arrays
+    """
+    if type(left) != type(right):
+        return False
+    elif isinstance(left, (pd.Series, pd.DataFrame, pd.Series)):
+        return left.equals(right)
+    elif isinstance(left, np.ndarray):
+        return np.array_equal(left, right, equal_nan=True)
+    else:
+        return left == right
