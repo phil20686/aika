fc3c19b980530df80f28be9879b454a4263d3610
Philip Stephens <phil_20686@hotmail.com>
2022-12-19 16:45:39 +0000

    foo

--- libs/utilities/setup.py
+++ libs/utilities/setup.py
@@ -1,30 +1,33 @@
-from pathlib import Path
-
-from pkg_resources import parse_requirements
-from setuptools import find_namespace_packages, setup
-
-parent_dir = Path(__file__).parent
-
-
-def get_requirements(path: Path):
-    with open(path) as fobj:
-        return [str(req) for req in parse_requirements(fobj.read())]
-
-
-setup(
-    name="aika-utilities",
-    setup_requires=["setuptools_scm"],
-    use_scm_version=dict(
-        root="../..",
-        relative_to=__file__,
-        write_to_template='__version__ = "{version}"',
-        write_to=parent_dir / "src/aika/utilities/_version.py",
-    ),
-    packages=find_namespace_packages("src", include=["aika.*"]),
-    package_dir={"": "src"},
-    install_requires=[
-        "pandas>=1.2.0",
-        "numpy",
-    ],
-    extras_require=dict(test=["pytest"]),
-)
+# from pathlib import Path
+#
+# from pkg_resources import parse_requirements
+# from setuptools import find_namespace_packages, setup
+#
+# parent_dir = Path(__file__).parent
+#
+#
+# def get_requirements(path: Path):
+#     with open(path) as fobj:
+#         return [str(req) for req in parse_requirements(fobj.read())]
+#
+#
+# setup(
+#     name="aika-utilities",
+#     setup_requires=["setuptools_scm"],
+#     use_scm_version=dict(
+#         root="../..",
+#         relative_to=__file__,
+#         write_to_template='__version__ = "{version}"',
+#         write_to=parent_dir / "src/aika/utilities/_version.py",
+#     ),
+#     packages=find_namespace_packages("src", include=["aika.*"]),
+#     package_dir={"": "src"},
+#     install_requires=[
+#         "pandas>=1.2.0",
+#         "numpy",
+#     ],
+#     extras_require=dict(test=["pytest"]),
+# )
+import setuptools
+if __name__ == "__main__":
+    setuptools.setup()
